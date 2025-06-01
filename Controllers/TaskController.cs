0ea780b026e9d313fa561386e8cfd9484bb81d54
TBrush123 <dayren.ermekbaev@gmail.com>
2025-06-02 00:47:49 +0500

    Made base of the site

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using ToDoList.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly AppDbContext _context;
+        public TaskController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var tasks = await _context.Tasks.ToListAsync();
+            return View(tasks);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Models.Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Tasks.Add(task);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(task);
+        }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ToDoList.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime DueDate { get; set; }
+
+    }
+}
